4d00470956cae9b551c5c841cbae8e4308a68e3e
laurent.gantel <laurent.gantel@hesge.ch>
2025-12-14 18:15:18 +0100

    tools - Make openocd_launch script more generic

--- pico/workspace/openocd_launch.sh
+++ pico/workspace/openocd_launch.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-sudo openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000" -c "program build/ble_sofa_app/ble_sofa_app.elf verify reset exit"
+function usage() {
+    echo "> Usage: $0 <app_path>"
+    echo "    app_path: Path to the app to flash from the build directory"
+    exit 1
+}
+
+if [ "$#" -ne 1 ]; then
+    usage
+fi
+
+APP_PATH=$1
+if [ ! -d "$APP_PATH" ]; then
+    echo "# $APP_PATH does not exist"
+    usage
+fi
+
+APP_NAME=$(basename $APP_PATH)
+
+echo "> APP_PATH: $APP_PATH"
+echo "> APP_NAME: $APP_NAME"
+
+sudo openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000" -c "program build/$APP_PATH/$APP_NAME.elf verify reset exit"
+
+echo "> Done"
